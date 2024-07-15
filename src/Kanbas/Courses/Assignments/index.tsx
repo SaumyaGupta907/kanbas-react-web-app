03531e67b29b2b619da7f27155142142008e61e3
saumya <saumya.1126@gmail.com>
2024-07-16 02:32:08 +0530

    assignment 2

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,42 +1,71 @@
+import { IoMdSearch } from "react-icons/io";
+import AssignmentControlButtons from "./AssignmentControlButtons";
+import AssignControlButtons from  "./AssignControlButtons";
+import { BsGripVertical } from "react-icons/bs";
+
 export default function Assignments() {
     return (
         <div id="wd-assignments">
-            <input id="wd-search-assignment"
-                   placeholder="Search for Assignments" />
+            <IoMdSearch /><input id="wd-search-assignment"
+                   placeholder="Search" />
             <button id="wd-add-assignment-group">+ Group</button>
             <button id="wd-add-assignment">+ Assignment</button>
-            <h3 id="wd-assignments-title">
+            <ul id="wd-assignments" className="list-group rounded-0">
+                <li className="wd-assignment list-group-item p-0 mb-5 fs-5 border-gray">
+            <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" />
+            
+            
                 ASSIGNMENTS 40% of Total <button>+</button>
-            </h3>
-            <ul id="wd-assignment-list">
-                <li className="wd-assignment-list-item">
+                <AssignmentControlButtons />
+                </div>
+
+
+                <ul className="wd-assignments list-group rounded-0">
+
+
+              <li className="wd-assignment list-group-item p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
                     <a className="wd-assignment-link"
                        href="#/Kanbas/Courses/1234/Assignments/123">
                         A1 - ENV + HTML
                     </a>
-                    <p>Multiple Modules | <b>Not available until</b> May 6 at 12:00am | <br/><b>Due </b>
+                    <AssignControlButtons />
+              
+                    <p><span className="text-danger">Multiple Modules</span> | <b>Not available until</b> May 6 at 12:00am | <br/><b>Due </b>
                       May 13 at 11:59pm | 100pts
                     </p>
                 </li>
-                <li className="wd-assignment-list-item">
-                    {/* Complete On Your Own */}
-                    <a
-                    href="#/Kanbas/Courses/1234/Assignments/123">
-                    A2 - CSS + BOOTSTRAP
+
+                <li className="wd-assignment list-group-item p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
+                    <a className="wd-assignment-link"
+                       href="#/Kanbas/Courses/1234/Assignments/123">
+                        A2 - CSS + BOOTSTRAP
                     </a>
-                    <p>Multiple Modules | <b>Not available until</b> May 13 at 12:00am | <br/><b>Due </b>
-                        May 20 at 11:59pm | 100pts
+                    <AssignControlButtons />
+              
+                    <p><span className="text-danger">Multiple Modules</span> | <b>Not available until</b> May 13 at 12:00am | <br/><b>Due </b>
+                    May 20 at 11:59pm | 100pts
                     </p>
                 </li>
-                <li className="wd-assignment-list-item">
+
+                <li className="wd-assignment list-group-item p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
                     <a className="wd-assignment-link"
                        href="#/Kanbas/Courses/1234/Assignments/123">
                         A3 - JAVASCRIPT + REACT
                     </a>
-                    <p>Multiple Modules | <b>Not available until</b> May 20 at 12:00am | <br/><b>Due </b>
-                        May 27 at 11:59pm | 100pts
+                    <AssignControlButtons />
+              
+                    <p><span className="text-danger">Multiple Modules</span> | <b>Not available until</b> May 20 at 12:00am | <br/><b>Due </b>
+                    May 27 at 11:59pm | 100pts
                     </p>
                 </li>
-            </ul>
+
+                    </ul>
+                </li>
+                </ul>
+            
         </div>
     );}
--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,70 +1,60 @@
-export default function Modules() {
+import ModulesControls from "./ModulesControls";
+import LessonControlButtons from "./LessonControlButtons";
+import ModuleControlButtons from "./ModuleControlButtons";
+import { BsGripVertical } from "react-icons/bs";
+
+ export default function Modules() {
     return (
-        <div>
-            {/* Collapse All button, View Progress button, etc. */}
+ 
+    
+        <div id="wd-modules">
+        <ModulesControls /><br /><br /><br /><br />
+        <ul id="wd-modules" className="list-group rounded-0">
+          <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+            <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" />
+              Week 1
+              <ModuleControlButtons />
+            </div>
+            <ul className="wd-lessons list-group rounded-0">
+              <li className="wd-lesson list-group-item p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
+                LEARNING OBJECTIVES
+                <LessonControlButtons />
+              </li>
+              <li className="wd-lesson list-group-item p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
+                Introduction to the course
+                <LessonControlButtons />
 
-            <button id="wd-collapse-button" type="button">
-                Collapse All
-            </button>
-            <button id="wd-progress-button" type="button">
-                View Progress
-            </button>
-            <button id="wd-progress-all-button" type="button">
-                Publish All
-            </button>
-            <button id="wd-module-button" type="button">
-                Module
-            </button>
-
-            <ul id="wd-modules">
-                <li className="wd-module">
-                    <div className="wd-title">Week 1</div>
-                    <ul className="wd-lessons">
-                        <li className="wd-lesson">
-                            <span className="wd-title">LEARNING OBJECTIVES</span>
-                            <ul className="wd-content">
-                                <li className="wd-content-item">Introduction to the course</li>
-                                <li className="wd-content-item">Learn what is Web Development</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <span className="wd-title">READING</span>
-                            <ul className="wd-content">
-                                <li className="wd-content-item">Full Stack Developer - chapter 1 - Introduction</li>
-                                <li className="wd-content-item">Full Stack Developer - chapter 2 - Creating User</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <span className="wd-title">SLIDES</span>
-                            <ul className="wd-content">
-                                <li className="wd-content-item">Introduction to Web Development</li>
-                                <li className="wd-content-item">Creating an HTTP server with Node.js</li>
-                                <li className="wd-content-item">Creating a React Application</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li className="wd-module">
-                    <div className="wd-title">Week 2</div>
-                    <ul className="wd-lessons">
-                        <li className="wd-lesson">
-                            <span className="wd-title">LEARNING OBJECTIVES</span>
-                            <ul className="wd-content">
-                                <li className="wd-content-item">Learn how to create user interfaces with HTML</li>
-                                <li className="wd-content-item">Deploy the assignment to Netlify</li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul className="wd-lessons">
-                        <li className="wd-lesson">
-                            <span className="wd-title">SLIDES</span>
-                            <ul className="wd-content">
-                                <li className="wd-content-item">Introduction to HTML and the DOM</li>
-                                <li className="wd-content-item">Formatting Web content with Headings</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
+              </li>
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                Learn what is Web Development
+              </li>
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                LESSON 1
+              </li>
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                LESSON 2
+              </li>
             </ul>
-        </div>
-    );}
+          </li>
+          <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+            <div className="wd-title p-3 ps-2 bg-secondary">
+              Week 2
+            </div>
+            <ul className="wd-lessons list-group rounded-0">
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                LEARNING OBJECTIVES
+              </li>
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                LESSON 1
+              </li>
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                LESSON 2
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      );}
