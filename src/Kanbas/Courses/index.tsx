9bda937df6140225d07f9ca6589327128fe7f9f1
saumya <saumya.1126@gmail.com>
2024-07-16 00:28:06 +0530

    Lab

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -4,18 +4,18 @@ import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import Home from "./Home";
 import { Navigate, Route, Routes } from "react-router";
+import { FaAlignJustify } from "react-icons/fa";
 
 export default function Courses() {
     return (
         <div id="wd-courses">
-            <h2>Course 1234</h2>
+            <h2 className="text-danger"><FaAlignJustify className="me-4 fs-4 mb-1" />
+            Course 1234</h2>
             <hr />
-            <table>
-                <tr>
-                    <td valign="top">
+            
                         <CoursesNavigation />
-                    </td>
-                    <td valign="top">
+                    
+                    <div>
                         <Routes>
                             <Route path="/" element={<Navigate to="Home" />} />
                             <Route path="Home" element={<Home />} />
@@ -23,9 +23,8 @@ export default function Courses() {
                             <Route path="Assignments" element={<Assignments/>} />
                             <Route path="Assignments/:id" element={<AssignmentEditor/>} />
                         </Routes>
-                    </td>
-                </tr>
-            </table>
+                        </div>
+                
 
         </div>
     );}
