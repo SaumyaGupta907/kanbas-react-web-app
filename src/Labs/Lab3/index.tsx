b34125bbab0edb01814f3b552bb585b8c4fd4cd8
SaumyaGupta907 <saumya.1126@gmail.com>
2024-07-28 13:06:52 -0700

    Assignment 4

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -37,7 +37,6 @@ export default function Lab3() {
     
     return (
       <div id="wd-lab3" className="container-fluid">
-        
         <h3>Lab 3</h3>
         <VariablesAndConstants />
         <VariableTypes />
